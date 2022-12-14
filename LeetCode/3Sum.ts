a44d86a8f1dd883388b824a8e54ffbdaaef8527a
chisan01 <chisan2001@gmail.com>
2022-12-14 15:49:47 +0900

    [LeetCode] 3Sum / 풀이 참고해서 해결
    모든 nums[i] + nums[j]를 구해서 map에 저장해두려면 최대 3000 * 3000 가지 경우의 수를 저장하게 되는데, 반대로 nums[k]를 map에 저장해서 사용하면 시간 복잡도는 동일하게 가져가면서, 메모리는 더 적게 사용할 수 있다. (왜 이 생각을 못했지..)
    
    풀이들을 구경해봤더니 nums를 정렬해서 two-pointer나 binary-search를 이용해서 k를 구하는 방법도 있다.

--- LeetCode/3Sum.ts
+++ LeetCode/3Sum.ts
@@ -1,33 +1,18 @@
-class Pair {
-  idx1: number;
-  idx2: number;
-  sum: number;
-
-  constructor(idx1: number, idx2: number, sum: number) {
-      this.idx1 = idx1;
-      this.idx2 = idx2;
-      this.sum = sum;
-  }
-}
-
 function threeSum(nums: number[]): number[][] {
-  let pairsWhichSumIs = new Map<number, Set<Pair>>();
-  for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length; j++) {
-          const curPair = new Pair(i, j, nums[i] + nums[j]);
-          if(!pairsWhichSumIs.has(curPair.sum)) pairsWhichSumIs.set(curPair.sum, new Set());
-          const pairsWithSameSum = pairsWhichSumIs.get(curPair.sum);
-          pairsWithSameSum.add(curPair);
-          pairsWhichSumIs.set(curPair.sum, pairsWithSameSum);
-      }
-  }
-  const result: Set<string> = new Set();
-  for (let i = 0; i < nums.length; i++) {
-      if(!pairsWhichSumIs.has(-nums[i])) continue;
-      for (const candidatePair of pairsWhichSumIs.get(-nums[i])) {
-          if(candidatePair.idx1 == i || candidatePair.idx2 == i) continue;
-          result.add(JSON.stringify([nums[i], nums[candidatePair.idx1], nums[candidatePair.idx2]].sort()));
-      }
-  }
-  return Array.from(result).map(it => JSON.parse(it));
+    const map = new Map<number, number>();
+    for (let k = 0; k < nums.length; k++) {
+        map.set(nums[k], k);
+    }
+
+    const result = new Set<string>();
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            const sum = nums[i] + nums[j];
+            if (!map.has(-sum)) continue;
+            const k = map.get(-sum);
+            if (k == i || k == j) continue;
+            result.add(JSON.stringify([nums[i], nums[j], nums[k]].sort()));
+        }
+    }
+    return Array.from(result).map(it => JSON.parse(it));
 }
