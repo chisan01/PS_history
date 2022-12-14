8391c13bf6257ccf608abcb0eaf80f58b4d57277
chisan01 <chisan2001@gmail.com>
2022-12-14 09:57:37 +0900

    [LeetCode] Medium ContainerWithMostOCWater / 성공(재시도)
    어제 풀이의 접근방식이 아예 잘못되어서 새로운 방식으로 다시 풀어서 성공했다

--- LeetCode/container-with-most-water.ts
+++ LeetCode/container-with-most-water.ts
@@ -1,56 +1,33 @@
-class Point {
-  idx: number;
-  height: number;
+let height: number[];
+let leftIdx: number;
+let rightIdx: number;
+let maxSize: number;
 
-  constructor(idx: number, height: number) {
-      this.idx = idx;
-      this.height = height;
-  }
+function maxArea(_height: number[]): number {
+    init(_height);
+    while (leftIdx < rightIdx) {
+        updateMaxSize();
+        updateIdxs();
+    }
+    return maxSize;
 }
 
-let ret: number;
-let startPointCandidates: Point[];
-let startPoint: Point;
-
-function candidateExists(): boolean {
-  return startPointCandidates.length !== 0
+function init(_height: number[]) {
+    height = _height;
+    leftIdx = 0;
+    rightIdx = height.length - 1;
+    maxSize = 0;
 }
 
-function maxArea(height: number[]): number {
-  init(height);
-  height.forEach((curHeight, curIdx) => {
-      const curPoint = checkPoint(curIdx, curHeight);
-      if (startPoint.height >= curPoint.height) return;
-      updateStartPoint(curPoint);
-  })
-  return ret;
+function updateMaxSize() {
+    maxSize = Math.max(maxSize, getCurSize());
 }
 
-function init(height: number[]) {
-  ret = 0;
-  startPoint = new Point(0, height[0]);
-  startPointCandidates = [];
+function getCurSize(): number {
+    return (rightIdx - leftIdx) * Math.min(height[leftIdx], height[rightIdx]);
 }
 
-function checkPoint(idx: number, height: number) {
-  const curPoint = new Point(idx, height);
-  ret = Math.max(ret, calSizeOf(startPoint, curPoint));
-  if (curPoint.height > startPoint.height) {
-      startPointCandidates.push(curPoint);
-  }
-  return curPoint;
-}
-
-function updateStartPoint(curPoint: Point) {
-  for (let candidate of startPointCandidates) {
-      const candidateSize = calSizeOf(candidate, curPoint);
-      if (ret > candidateSize) continue;
-      startPoint = candidate;
-      ret = candidateSize;
-  }
-}
-
-function calSizeOf(startPoint: Point, endPoint: Point): number {
-  if (startPoint.idx > endPoint.idx) return calSizeOf(endPoint, startPoint);
-  return (endPoint.idx - startPoint.idx) * Math.min(startPoint.height, endPoint.height);
+function updateIdxs() {
+    if (height[leftIdx] > height[rightIdx]) rightIdx--;
+    else leftIdx++;
 }
