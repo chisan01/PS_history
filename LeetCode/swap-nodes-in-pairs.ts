11e7409f58c2f46762db93989e786f5a6bf4485d
chisan01 <chisan2001@gmail.com>
2022-12-17 19:57:27 +0900

    [LeetCode] SwapNodesInPairs / 리팩토링
    풀이를 좀 더 깔끔하게 작성하려고 고민하다가 재귀함수를 통해 변수 사용을 줄이고 가독성을 높임

--- LeetCode/swap-nodes-in-pairs.ts
+++ LeetCode/swap-nodes-in-pairs.ts
@@ -1,25 +1,14 @@
 function swapPairs(head: ListNode | null): ListNode | null {
-  let isFirst = true;
-  let prevLastNode: ListNode;
-  let firstNode: ListNode | null = head;
-  let secondNode: ListNode | null = null;
+  return swapPairRecursively(head);
+}
 
-  while(firstNode !== null) {
-      secondNode = firstNode.next;
-      if(secondNode === null) break;
+function swapPairRecursively(firstNode: ListNode | null): ListNode | null {
+  if(firstNode === null) return firstNode;
+  const secondNode = firstNode.next;
+  if(secondNode === null) return firstNode;
 
-      firstNode.next = secondNode.next;
-      secondNode.next = firstNode;
-      if(isFirst) {
-          head = secondNode;
-          isFirst = false;
-      } else {
-          prevLastNode.next = secondNode;
-      }
-
-      prevLastNode = firstNode;
-      firstNode = firstNode.next;
-      secondNode = null;
-  }
-  return head;
+  const firstNodeOfNextPair = secondNode.next;
+  secondNode.next = firstNode;
+  firstNode.next = swapPairRecursively(firstNodeOfNextPair);
+  return secondNode;
 }
