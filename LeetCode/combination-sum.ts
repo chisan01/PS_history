845fdc11fe1a0dc605efa445d5318a9e1f2ef32b
chisan01 <chisan2001@gmail.com>
2022-12-26 15:10:00 +0900

    [LeetCode] Medium 39 / 리팩토링
    코드 리뷰를 하면서 코드 가독성을 좀 더 개선할 수 있을 것 같아서 리팩토링 수행

--- LeetCode/combination-sum.ts
+++ LeetCode/combination-sum.ts
@@ -1,47 +1,48 @@
 function combinationSum(candidates: number[], target: number): number[][] {
-    const combinationSum = new CombinationSum(candidates);
-    return combinationSum.getCombinationsFor(target);
+    const combinationSum = new CombinationForSum(candidates);
+    return combinationSum.getCombinations(target);
 }
 
-class CombinationSum {
-    combinationsFor: Map<number, number[][]>;
-    candidates: number[];
+class CombinationForSum {
+    combinationsWhichSumIs: Map<number, number[][]>;
+    candidateNums: number[];
 
     constructor(candidates: number[]) {
-        this.combinationsFor = new Map<number, number[][]>();
-        this.candidates = candidates;
+        this.combinationsWhichSumIs = new Map<number, number[][]>();
+        this.candidateNums = candidates;
     }
 
-    getCombinationsFor(target: number): number[][] {
-        if (this.combinationsFor.has(target)) {
-            return this.combinationsFor.get(target);
+    getCombinations(sum: number): number[][] {
+        if (!this.combinationsWhichSumIs.has(sum)) {
+            const combinations = this.calDistinctCombinations(sum);
+            this.combinationsWhichSumIs.set(sum, combinations);
         }
-        const combinations = this.getAllCombinationsFor(target);
-        const distinctCombinations = distinct(combinations);
-        this.combinationsFor.set(target, distinctCombinations);
-        return distinctCombinations;
+        return this.combinationsWhichSumIs.get(sum);
     }
 
-    private getAllCombinationsFor(target: number): number[][] {
+    private calDistinctCombinations(sum: number): number[][] {
+        const combinations = this.calCombinations(sum);
+        return removeDuplicates(combinations);
+    }
+
+    private calCombinations(sum: number): number[][] {
         const combinations: number[][] = [];
-        this.candidates.forEach(candidate => {
-            if (candidate === target) {
-                combinations.push([candidate]);
+        this.candidateNums.forEach(curNum => {
+            if (curNum === sum) {
+                combinations.push([curNum]);
             }
-            if (candidate >= target) {
+            if (curNum >= sum) {
                 return;
             }
-            const nextTarget = target - candidate;
-            const combinationsOfNextTarget = this.getCombinationsFor(nextTarget);
-            combinationsOfNextTarget
-                .map(value => [...value, candidate])
-                .forEach(value => combinations.push(value));
+            this.getCombinations(sum - curNum)
+                .map(prevCombination => [...prevCombination, curNum])
+                .forEach(curCombination => combinations.push(curCombination));
         });
         return combinations;
     }
 }
 
-function distinct(nums: number[][]): number[][] {
+function removeDuplicates(nums: number[][]): number[][] {
     const distinctSet = new Set([...nums].map(value => JSON.stringify(value.sort())));
     return [...distinctSet].map(value => JSON.parse(value));
 }
